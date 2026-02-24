561f9aa6638c8b64c19ea87fa44d23217afc76df
Kinga Jutrzenka Trzebiatowska <jutrzenkatkinga@gmail.com>
2026-02-24 21:18:52 +0100

    Add files via upload

---
+++ kurs.R
@@ -0,0 +1,29 @@
+
+#ax^2 + bx + c = 0
+#x^2 + x - 1 = 0
+
+a <- 1
+b <- 1
+c <- -1
+
+#it's the same
+
+print(a)
+print(b)
+print(c)
+
+a
+b
+c
+
+#show every objects
+
+ls()
+
+#delta
+
+delta <- sqrt(b^2 - 4*a*c)
+x1 <- ((-b + delta))/(2*a)
+x2 <- ((-b - delta))/(2*a)
+x1
+x2
