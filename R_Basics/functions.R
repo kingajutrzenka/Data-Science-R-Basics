3da85c90ab366b415ddc484a0ede5b11a4968780
Kinga Jutrzenka Trzebiatowska <jutrzenkatkinga@gmail.com>
2026-02-25 20:52:09 +0100

    Add files via upload

---
+++ R_Basics/functions.R
@@ -0,0 +1,30 @@
+#Functions
+
+ls #function
+ls() #use function
+
+log(8)
+log
+
+exp(1)
+#2.718..
+log(2.718)
+#1
+
+#nested functions:
+
+log(exp(1))
+# first exp(1), second log...
+
+#Help:
+
+help("log") #all infomations
+args(log) #what we need to use
+
+log(8, base=2)
+log(8, 2)
+
+2^3
+?"+" #all arithmetic operators
+
+data()
